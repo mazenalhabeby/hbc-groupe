8da7292eaa5d1cd6854780b23322b523e610414c
mazenalhabeby <a.mazen.dessouky@gmail.com>
2025-08-18 00:20:37 +0200

    remove shops and dashboard

--- frontend/app/[locale]/(dashboard)/admin/shipping/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Page() {
-  return (
-    <React.Fragment>
-      <main className="flex-1 overflow-y-auto px-6">shipping</main>
-    </React.Fragment>
-  )
-}
